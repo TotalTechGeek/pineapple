18053a10162d010a7ae9df8e9ed3759e6bfe5048
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2022-11-30 14:45:17 -0600

    Fix a bad type

--- outputs.js
+++ outputs.js
@@ -8,7 +8,7 @@ const format = text => text.includes('\n') ? `\n${text}\n` : text
  * Announces that a test is being skipped due to it not being exported.
  * @param {string} name
  * @param {string} file
- * @param {{ type: string, text: string, lineNo: number }} tags
+ * @param {{ type: string, text: string, lineNo: number }[]} tags
  */
 export function skippingTest (name, file, tags) {
   if (process.env.OUTPUT_FORMAT === 'CONSOLE') {
