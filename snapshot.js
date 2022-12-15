3d3b4be7198997ed288add664ca0e8b5f510b4c9
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2022-12-14 20:50:39 -0600

    Resolve issue with negative infinity

--- snapshot.js
+++ snapshot.js
@@ -41,9 +41,7 @@ export function snapshot (file = './pineapple-snapshot') {
  * @test 5 returns "5"
  * @test true returns "true"
  * @test Infinity returns "Infinity"
- *
- * This next one should be fixed.
- * @test @__fails__ -Infinity returns "-Infinity"
+ * @test -Infinity returns "-Infinity"
  *
  * @test null returns "null"
  * @test undefined returns "undefined"
