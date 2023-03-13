c4cf2cc4ef8bdaa01fec152ff47f48db4800fd1c
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2023-03-12 23:12:31 -0500

    Add circular to test out fallback

--- annotationSamples.test.js
+++ annotationSamples.test.js
@@ -1,9 +1,14 @@
 
+const circular = { }
+circular.circle = circular
+
 /**
  * @test { name: 'Jesse', age: 25, email: 'jesse.daniel.mitchell@gmail.com' } returns
  * @test { name: 'Bob', age: 37, email: 'bob@bobwarehouse.com' } returns
  */
 export function commentTap (input) {
+  // eslint-disable-next-line no-unused-vars
+  const circle = circular // ?
   return {
     message: `Hello ${
         input // ? @.email
